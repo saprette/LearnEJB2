025c38ac8f7bf9add119da022244d23e5bd11326
Prette <samuel@prette.fr>
2012-06-10 19:14:48 +0200

    LearnEJB-1 Add JBoss

--- src/servlet/SalutationServlet.java
+++ src/servlet/SalutationServlet.java
@@ -36,7 +36,7 @@ public class SalutationServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>" +
-                    salutationBean.getFormalSalutation("Sherlock Holmestetst") +
+                    salutationBean.getFormalSalutation("Sherlock Holmestetst change") +
                     "</h1>");
             out.println("</body>");
             out.println("</html>");
